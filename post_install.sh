77611b89c0c44e997c18cbb51ceb5597c1f62a3a
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-03-03 19:05:39 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -7,6 +7,8 @@ start_date=$(date)
 
 chmod 755 ./start_web.sh
 
+# ***** postgresql *****
+
 postgres_user=$(echo ${DATABASE_URL} | awk -F':' '{print $2}' | sed -e 's/\///g')
 postgres_password=$(echo ${DATABASE_URL} | grep -o '/.\+@' | grep -o ':.\+' | sed -e 's/://' | sed -e 's/@//')
 postgres_server=$(echo ${DATABASE_URL} | awk -F'@' '{print $2}' | awk -F':' '{print $1}')
@@ -28,6 +30,37 @@ CREATE TABLE t_file (
 __HEREDOC__
 cat /tmp/sql_result.txt
 
+# ***** aria2 *****
+
+cd /tmp
+
+wget https://github.com/aria2/aria2/releases/download/release-1.33.1/aria2-1.33.1.tar.bz2
+
+tar xvf aria2-1.33.1.tar.bz2
+
+cd aria2-1.33.1
+
+./configure --help
+./configure --prefix=/tmp/usr
+make -j2
+make install
+
+# ***** tar *****
+
+cd /tmp
+time tar -jcf usr_aria2.tar.bz2 usr
+
+base64 -w 0 usr_aria2.tar.bz2 > usr_aria2.tar.bz2.base64.txt
+
+ls -lang
+
+set +x
+base64_text=$(cat /tmp/usr_gettext.tar.bz2.base64.txt)
+
+psql -U ${postgres_user} -d ${postgres_dbname} -h ${postgres_server} > /tmp/sql_result.txt << __HEREDOC__
+INSERT INTO t_files (file_name, file_base64_text) VALUES ('usr_aria2.tar.bz2', '${base64_text}');
+__HEREDOC__
+set -x
 
 echo ${start_date}
 date
