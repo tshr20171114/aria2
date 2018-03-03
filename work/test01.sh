c9b9b52128f3c0ed1214f6093b399ae0d905ed08
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-03-03 21:37:07 +0900

    Update test01.sh

--- work/test01.sh
+++ work/test01.sh
@@ -9,3 +9,14 @@ echo ${len}
 split=$(($len / 20))
 
 echo ${split}
+
+range1=0
+range2=$(($split - 1))
+
+for ((i=0; i < 19; i++)); do
+  echo $i $range1 $range2
+  range1=$(($range2 + 1))
+  range2=$(($range2 + $split))
+done
+
+echo $range1
