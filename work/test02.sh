4cb81c7b5ca31580f1615af3f14cfe8f6f9fdbff
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-03-04 02:29:12 +0900

    Update test02.sh

--- work/test02.sh
+++ work/test02.sh
@@ -32,6 +32,29 @@ wait
 
 cat file.* > filedata.dat
 
-base64 -w 0 filedata.dat filedata.dat.base64.txt
+base64 -w 0 filedata.dat > filedata.dat.base64.txt
 
 ls -lang
+
+# ***** postgresql *****
+
+postgres_user=$(echo ${DATABASE_URL} | awk -F':' '{print $2}' | sed -e 's/\///g')
+postgres_password=$(echo ${DATABASE_URL} | grep -o '/.\+@' | grep -o ':.\+' | sed -e 's/://' | sed -e 's/@//')
+postgres_server=$(echo ${DATABASE_URL} | awk -F'@' '{print $2}' | awk -F':' '{print $1}')
+postgres_dbname=$(echo ${DATABASE_URL} | awk -F'/' '{print $NF}')
+
+export PGPASSWORD=${postgres_password}
+
+psql -U ${postgres_user} -d ${postgres_dbname} -h ${postgres_server} > /tmp/sql_result.txt << __HEREDOC__
+DELETE
+  FROM t_files
+ WHERE file_name = 'filedata.dat'
+__HEREDOC__
+
+set +x
+base64_text=$(cat /tmp/${filename}.tar.bz2.base64.txt)
+
+psql -U ${postgres_user} -d ${postgres_dbname} -h ${postgres_server} > /tmp/sql_result.txt << __HEREDOC__
+INSERT INTO t_files (file_name, file_base64_text) VALUES ('${filename}.tar.bz2', '${base64_text}');
+__HEREDOC__
+set -x
