e459af6a74bf2c0b041e61b9627d3002310f0206
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-03-03 23:04:53 +0900

    Create test02.sh

---
+++ work/test02.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -x
+
+len=$(curl -I $url | grep Content-Length | tr '\r' ' ' | awk '{print $2}')
+
+echo ${len}
+
+split=$(($len / 50))
+
+echo ${split}
+
+range1=0
+range2=$(($split - 1))
+
+for ((i=0; i < 49; i++)); do
+  echo $i $range1 $range2
+  curl -r ${range1}-${range2} -o file.$i $url &
+  range1=$(($range2 + 1))
+  range2=$(($range2 + $split))
+done
+
+echo $range1
+curl -r ${range1}-${len} -o file.49 $url &
+
+wait
+
+ls -lang
