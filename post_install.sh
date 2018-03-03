91d86615472dcc6b7c07ec07883d6893da0a969f
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-03-03 19:52:56 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -38,6 +38,12 @@ SELECT file_base64_text
  WHERE file_name = 'usr_aria2.tar.bz2'
 __HEREDOC__
 
+psql -U ${postgres_user} -d ${postgres_dbname} -h ${postgres_server} > /tmp/sql_result.txt << __HEREDOC__
+DELETE
+  FROM t_files
+ WHERE file_name = 'usr_aria2.tar.bz2'
+__HEREDOC__
+
 # ***** /tmp/usr *****
 
 cd /tmp
@@ -52,6 +58,61 @@ tar xf /tmp/usr.tar.bz2 -C /tmp/usr --strip=1
 
 ls -Rlang usr
 
+# ***** env *****
+
+export HOME2=${PWD}
+export PATH="/tmp/usr2/bin:/tmp/usr/bin:${PATH}"
+export LD_LIBRARY_PATH=/tmp/usr/lib
+
+export CFLAGS="-march=native -O2"
+export CXXFLAGS="$CFLAGS"
+
+parallels=$(grep -c -e processor /proc/cpuinfo)
+
+# ***** ccache *****
+
+cd /tmp
+
+wget https://www.samba.org/ftp/ccache/ccache-3.3.4.tar.gz
+tar xf ccache-3.3.4.tar.gz
+cd ccache-3.3.4
+./configure --help
+./configure --prefix=/tmp/usr2 --mandir=/tmp/man --docdir=/tmp/doc
+make -j${parallels}
+make install
+
+cd /tmp/usr2/bin
+ln -s ccache gcc
+ln -s ccache g++
+ln -s ccache cc
+ln -s ccache c++
+
+mkdir -m 777 /tmp/ccache
+export CCACHE_DIR=/tmp/ccache
+
+time psql -U ${postgres_user} -d ${postgres_dbname} -h ${postgres_server} > /tmp/sql_result.txt << __HEREDOC__
+SELECT file_base64_text
+  FROM t_files
+ WHERE file_name = 'ccache_cache.aria2.tar.bz2'
+__HEREDOC__
+
+if [ $(cat /tmp/sql_result.txt | grep -c '(1 row)') -eq 1 ]; then
+  set +x
+  time echo $(cat /tmp/sql_result.txt | head -n 3 | tail -n 1) > /tmp/ccache_cache.tar.bz2.base64.txt
+  set -x
+  time base64 -d /tmp/ccache_cache.tar.bz2.base64.txt > /tmp/ccache_cache.tar.bz2
+  tar xf /tmp/ccache_cache.tar.bz2 -C /tmp/ccache --strip=1
+fi
+
+psql -U ${postgres_user} -d ${postgres_dbname} -h ${postgres_server} > /tmp/sql_result.txt << __HEREDOC__
+DELETE
+  FROM t_files
+ WHERE file_name = 'ccache_cache.aria2.tar.bz2'
+__HEREDOC__
+
+ccache -s
+ccache -z
+
 # ***** aria2 *****
 
 cd /tmp
@@ -64,10 +125,23 @@ cd aria2-1.33.1
 
 ./configure --help
 ./configure --prefix=/tmp/usr --mandir=/tmp/man --docdir=/tmp/doc
-make -j$(grep -c -e processor /proc/cpuinfo)
+make -j${parallels}
 make install
 
 # ***** tar *****
+
+cd /tmp
+time tar -jcf ccache_cache.tar.bz2 ccache
+
+base64 -w 0 ccache_cache.tar.bz2 > ccache_cache.tar.bz2.base64.txt
+
+set +x
+base64_text=$(cat /tmp/ccache_cache.tar.bz2.base64.txt)
+
+psql -U ${postgres_user} -d ${postgres_dbname} -h ${postgres_server} > /tmp/sql_result.txt << __HEREDOC__
+INSERT INTO t_files (file_name, file_base64_text) VALUES ('ccache_cache.aria2.tar.bz2', '${base64_text}');
+__HEREDOC__
+set -x
 
 cd /tmp
 
