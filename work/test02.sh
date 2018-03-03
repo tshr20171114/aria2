7cb48a8d4ecbf5e2b69964682adf51aa16b6984f
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-03-03 23:26:44 +0900

    Update test02.sh

--- work/test02.sh
+++ work/test02.sh
@@ -2,19 +2,23 @@
 
 set -x
 
+count=${1}
+
 len=$(curl -I $url | grep Content-Length | tr '\r' ' ' | awk '{print $2}')
 
 echo ${len}
 
-split=$(($len / 50))
+split=$(($len / ${count}))
 
 echo ${split}
 
 range1=0
 range2=$(($split - 1))
 
-for ((i=0; i < 49; i++)); do
-  suffix=$(printf "%02d" $i)
+loop_end=$(($count - 1))
+
+for ((i=0; i < ${loop_end}; i++)); do
+  suffix=$(printf "%03d" $i)
   echo $i $suffix $range1 $range2
   curl -r ${range1}-${range2} -o file.$suffix $url &
   range1=$(($range2 + 1))
@@ -22,7 +26,7 @@ for ((i=0; i < 49; i++)); do
 done
 
 echo $range1
-curl -r ${range1}-${len} -o file.49 $url &
+curl -r ${range1}-${len} -o file.${loop_end} $url &
 
 wait
 
