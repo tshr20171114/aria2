d6033624138be0c58ff7ad90285e6d4a1f98c766
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-03-14 20:35:48 +0900

    Create test03.sh

---
+++ test03.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -x
+
+count=20
+
+len=$(curl -I $url | grep Content-Length | tr '\r' ' ' | awk '{print $2}')
+
+echo ${len}
+
+split=$(($len / ${count}))
+
+echo ${split}
+
+range1=0
+range2=$(($split - 1))
+
+loop_end=$(($count - 1))
+
+tmp_dir=$(date "+%Y%m%d%H%M%S")
+
+mkdir 666 /tmp/${tmp_dir}
+
+for ((i=0; i < ${loop_end}; i++)); do
+  suffix=$(printf "%03d" $i)
+  echo $i $suffix $range1 $range2
+  curl -r ${range1}-${range2} -o /tmp/${tmp_dir}/file.$suffix $url &
+  range1=$(($range2 + 1))
+  range2=$(($range2 + $split))
+done
+
+echo $range1
+curl -r ${range1}-${len} -o file.${loop_end} $url &
+
+wait
+
+cat /tmp/${tmp_dir}/file.* > /tmp/filedata.dat
